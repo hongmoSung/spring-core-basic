6e1a773812fff2c836c9b9ace046d550550bb4e9
hongmo sung <tesung30@naver.com>
2022-02-02 10:17:08 +0900

    first commit

---
+++ src/main/java/hello/core/SpringCoreBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreBasicApplication.class, args);
+	}
+
+}
